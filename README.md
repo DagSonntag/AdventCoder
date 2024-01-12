071d102bc5e7c533c925e23550e32cb46f8c63f9
DS <ds@DSAir.localdomain>
2024-01-12 15:20:48 +0100

    Added advent_of_code_downloader

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# llm_aoc_benchmark
+# AdventCoder
 A repository for benchmarking different LLM models on advent of code problems
+
+
+## Purpose
+The purpose of this repository is to test and benchmark different LLM models in terms of their ability to code and solve 
+problems of different complexity. Some common benchmarks already exists for this purpose, and the models are often tested
+on these, but it is often unclear exactly what these benchmark tests are, and how they are performed (prompt input etc.).
+
+Moreover, it is often hard to get an overview of how the different models struggle with different types of problems, and
+compare them in a fair way side by side. 
+
+Finally it is also unclear if the benchmark problems somehow have slipped into the training data of the models, giving 
+them an unfair advantage over older models.
+
+Hence, the purpose of this repository is to create a framework for testing and benchmarking different models on a set of
+well known problems to coders. For this the Advent of code problems are used (https://adventofcode.com/), since many 
+coders solve these problems each year and therefor have a good overview of their complexity. 
+
+The advent of code is a (competitive) programming competition where a new two-part problem is released each day from the
+1st to the 25th of December. The problems are often related to each other, and the difficulty increases as the days go by.
+Each year a new set of problems are also released, so it is possible to test the models on new problems each year 
+(and see if they remember old problems from their training data). Finally we can also relate how well the models can 
+solve the problems to publicised data on how many humans that manage to solve them, and how long it takes them to do so.
+
+## Scope
+The scope of this framework is to:
+- Provide code to download and parse the advent of code problems and solutions
+- Provide code to run the different models on the problems and evaluate the found solutions
+- Provide basic implementations for the most common LLMs 
+- Provide an easy interface to add new models
+- Provide code to compare and visualize the results of the models
+
+What is not in scope:
+- Evaluate the code in terms of how well it is written, security, or other metrics
+- Evaluate how well the proposed solutions solve the problems in terms of time and memory (other than hard constraints)
+- Evaluate how "close" an incorrect solution is to a correct solution. Either it is correct or it is not.
+
+## Usage
+
+## Framework design
+
+## TODO:
---
+++ src/advent_of_code_downloaders/full_problem_downloader.py
@@ -0,0 +1,93 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import logging
+
+from pathlib import Path
+import os
+
+
+def download_advent_of_code_data(advent_of_code_website, year, data_folder_location, github_username=None,
+                                 github_password=None):
+    """
+    This script downloads all the problems and solutions from Advent of Code for a single year.
+    It does this by:
+    1. Opening a browser
+    2. Logging into the website
+    - For each day:
+        - Download the part of the website that contains the relevant text
+        - Split the text into the two parts
+        - For each part, save the correct response and remove it from the original text
+        - Save the text to a file (one for each day) in the data folder
+    - Close the browser
+
+    Limitations:
+    - Currently it uses the GitHub credentials (Google and twitter oath are also available) if provided, otherwise it
+    will require manual login in the browser window.
+    - Currently it assumes the problems have been solved in the past, or nothing is saved.
+    """
+    print("Starting data download")
+    save_folder_path = Path(data_folder_location) / str(year)
+    save_folder_path.mkdir(parents=True, exist_ok=True)
+
+    options = webdriver.ChromeOptions()
+    # start in full size
+    options.add_argument("--start-maximized")
+    # Note that we need to run in not headless mode to be able to log in using oauth (and two-factor authentication)
+
+    browser = webdriver.Chrome(options=options)
+    wait = WebDriverWait(browser, 10)
+    browser.get(advent_of_code_website + '/' + str(year) + '/auth/login')
+
+    # Sign in using oauth from GitHub if credentials are provided, else let the user sign in manually
+    if github_username is not None and github_password is not None:
+        browser.find_element(By.LINK_TEXT, "[GitHub]").click()
+        # type email
+        wait.until(EC.presence_of_element_located((By.ID, "login_field"))).send_keys(github_username)
+        # type email
+        wait.until(EC.presence_of_element_located((By.ID, "password"))).send_keys(github_password)
+        # click signin
+        wait.until(EC.presence_of_element_located((By.NAME, "commit"))).click()
+        # Here two-factor authentication is required and might have to be done manually.
+    # Wait until we are back at advent of code
+    print("Waiting for manually finishing login")
+    WebDriverWait(browser, 1000).until(EC.title_contains('Advent of Code'))
+    print("Login finished, continuing data download")
+
+    # For each day:
+    for day in range(1, 26):
+        # Download the part of the website that contains the relevant text
+        browser.get(f'{advent_of_code_website}/{year}/day/{day}')
+        question_text = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "day-desc")))
+
+        # Find the answers paragraphs with the text "Your puzzle answer was"
+        question_ans_paragraphs = browser.find_elements(By.XPATH, "//p[contains(text(), 'Your puzzle answer was')]")
+        # Find the <code> tag for each of these paragraphs and the text inside
+        question_ans = [q.find_element(By.TAG_NAME, "code").text for q in question_ans_paragraphs]
+
+        if len(question_ans) > len(question_text):
+            raise ValueError(f'There are more answers ({len(question_ans)}) than questions '
+                             f'({len(question_text)}) found for day {day}')
+
+        # Save the text to a file (one for each day) in the data folder
+        for i, ans in enumerate(question_ans):
+            logging.info(f"Saving day {day} part {i + 1}")
+            with open(save_folder_path / f"day_{day}_part_{i + 1}_text.txt", 'w') as f:
+                f.write(question_text[i].text)
+            with open(save_folder_path / f"day_{day}_part_{i + 1}.html", 'w') as f:
+                f.write(question_text[i].get_attribute('innerHTML'))
+            with open(save_folder_path / f"day_{day}_part_{i + 1}_ans.txt", 'w') as f:
+                f.write(ans)
+    browser.close()
+    print("Finished downloading data")
+
+
+if __name__ == '__main__':
+    # Get the user credentials to use from the environment variables
+    download_advent_of_code_data(
+        advent_of_code_website='https://adventofcode.com',
+        year=2023,
+        data_folder_location='data',
+        github_username=os.environ.get('GITHUB_USERNAME') if 'GITHUB_USERNAME' in os.environ else None,
+        github_password=os.environ.get('GITHUB_PASSWORD') if 'GITHUB_PASSWORD' in os.environ else None)
